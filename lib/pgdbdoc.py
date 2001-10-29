76fb0b5e75d0a7385fe108e50bdceba9742d8f37
Steve Purcell <steve@sanityinc.com>
2001-10-29 07:27:19 +0000

    Make choice of pg db module explicit
    git-svn-id: file:///tmp/out/trunk@12 aaa3d099-2f68-41d2-a801-defbee7ec4f3

--- lib/pgdbdoc.py
+++ lib/pgdbdoc.py
@@ -10,7 +10,7 @@
 #
 
 __author__ = 'Steve Purcell <stephen_purcell at yahoo dot com>'
-__version__ = '$Revision: 1.1.1.1 $'[11:-2]
+__version__ = '$Revision: 1.2 $'[11:-2]
 
 import dbdoc.dbdoc
 import dbdoc.pgschema
@@ -20,15 +20,17 @@ def usage_exit(progname, msg=None):
     if msg:
         print msg
         print
-    print "usage: %s connstring outdir [propsfile]" % progname
+    print "usage: %s connstring [-d dblib] outdir [propsfile]" % progname
     sys.exit(2)
 
 def main(argv):
     progname = os.path.basename(argv[0])
+    dblib = "pgdb"
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'h', ['help'])
+        opts, args = getopt.getopt(sys.argv[1:], 'hd:', ['help','dblib='])
         for opt, value in opts:
             if opt in ('-h', '--help'): usage_exit(progname)
+            if opt in ('-d', '--dblib'): dblib = value
     except getopt.error, e:
         usage_exit(progname, e)
     if len(args) == 3:
@@ -39,14 +41,10 @@ def main(argv):
     else:
         usage_exit(progname)
 
-    for lib in ('psycopg', 'PoPy', 'pgdb'):
-        try:
-            connector = __import__(lib)
-            break
-        except ImportError:
-            continue
-    if not connector:
-        print "couldn't find a pg access module"
+    try:
+        connector = __import__(dblib)
+    except ImportError:
+        print "couldn't find pg access module '%s'" % dblib
         sys.exit(1)
     conn = connector.connect(conn_string)
     schema = dbdoc.pgschema.PostgresSchema(conn, 'postgres')
@@ -54,14 +52,3 @@ def main(argv):
 
 if __name__ == '__main__':
     main(sys.argv)
-
-    #import pgdb
-    #conn = pgdb.connect(':postgres:postgres')
-
-    #import psycopg
-    #dsn = 'dbname=postgres user=postgres'
-    #conn = psycopg.connect(dsn)
-
-    #import PoPy
-    #dsn = 'dbname=postgres user=postgres'
-    #conn = PoPy.connect(dsn)
